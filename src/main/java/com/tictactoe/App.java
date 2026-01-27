c9b3c9aea08c57091aed66a9c585fb651f6cfaeb
Sukaina <sukisu91@gmail.com>
2026-01-27 15:33:40 +0100

    Player and board

--- src/main/java/com/tictactoe/App.java
+++ src/main/java/com/tictactoe/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+        Player player1 = new Player("Pedro", 'X') ;  
+        System.out.println(player1.getName());
+    }  
 }
--- src/main/java/com/tictactoe/Board.java
+++ src/main/java/com/tictactoe/Board.java
@@ -1,5 +1,21 @@
 package com.tictactoe;
 
 public class Board {
-    
+    private char [][] board;
+    public Board(){
+        board = new char [][]{
+           {' ',' ',' '},
+           {' ',' ',' '},
+           {' ',' ',' '},
+        };
+    }
+    public void showBoard(){
+        System.out.println();
+        for (int i= 0; i <3;i++){
+            System.out.println(" " + board + " ");
+        }
+
+    }
 }
+
+
